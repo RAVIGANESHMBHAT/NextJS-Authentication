eaf52b3bb35c098ef76cd52ba1ccce5ea25254a2
Raviganesh M <raviganeshmbhat999@gmail.com>
2024-04-06 11:05:46 +0530

    add me route to get logged in user details

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const userId = getDataFromToken(request);
+    const user = await User.findById(userId).select("-password");
+
+    if (!user) {
+      return (
+        NextResponse.json({
+          message: "User not found",
+        }),
+        { status: 404 }
+      );
+    }
+    return NextResponse.json({
+      data: user,
+      status: true,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
