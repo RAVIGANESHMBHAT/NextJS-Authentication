24b9bc05c21dd01f0fa1ae3667472ed1128887e6
Raviganesh M <raviganeshmbhat999@gmail.com>
2024-04-06 10:49:58 +0530

    add verifyemail route

---
+++ src/app/api/users/verifyemail/route.ts
@@ -0,0 +1,34 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/models/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const { token } = await request.json();
+
+    const user = await User.findOne({
+      verifyToken: token,
+      verifyTokenExpiry: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return NextResponse.json({ message: "Invalid token" }, { status: 400 });
+    }
+
+    user.isVerified = true;
+    user.verifyToken = undefined;
+    user.verifyTokenExpiry = undefined;
+
+    await user.save();
+
+    return NextResponse.json(
+      { message: "Email verified successfully.", success: true },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ message: error.message }), { status: 500 };
+  }
+}
